71cd0112ad9e39d3222226a2aa946416b31a903a
Ilfat Galiev <zBritva@users.noreply.github.com>
2018-07-04 17:30:42 +0300

    Fix interface. Apply ChromeHeadless. Update packages (#32)

--- lib/interactivityUtils.d.ts
+++
@@ -1,7 +0,0 @@
-import { shapesInterfaces } from "powerbi-visuals-utils-svgutils";
-import * as d3 from "d3";
-import { ISelectionHandler } from "./interactivityService";
-import IPoint = shapesInterfaces.IPoint;
-export declare function getPositionOfLastInputEvent(): IPoint;
-export declare function registerStandardSelectionHandler(selection: d3.Selection<any, any, any, any>, selectionHandler: ISelectionHandler): void;
-export declare function registerGroupSelectionHandler(group: d3.Selection<any, any, any, any>, selectionHandler: ISelectionHandler): void;
